d6c7e0164bbcd3ad417300af25630009c640ffff
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-23 11:42:13 +0100

    Implementing the Triplet Loss Function

--- Face_recognition_ConvNets.py
+++ Face_recognition_ConvNets.py
@@ -26,6 +26,38 @@ json_file = open('keras-facenet-h5/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
-model.load_weights('keras-facenet-h5/model.h5')
+model.load_weights('keras-facenet-h5/model.h5') # By using a 128-neuron fully connected layer as its last layer, the model ensures that the output is an encoding vector of size 128
+
 print(model.inputs)
 print(model.outputs)
+
+# Implementing the Triplet Loss Function
+
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+
+    Returns:
+    loss -- real number, value of the loss
+    """
+
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(np.array(anchor) - np.array(positive)), axis=-1)
+
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(np.array(anchor) - np.array(negative)), axis=-1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = pos_dist - neg_dist + alpha
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0))
+
+    return loss
