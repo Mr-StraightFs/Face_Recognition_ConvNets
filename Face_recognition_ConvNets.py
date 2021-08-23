dfa431367026d6377f0db63057fc8169e0d8a6df
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-23 12:11:16 +0100

    Implements face recognition for the office by finding who is the person on the image_path image

--- Face_recognition_ConvNets.py
+++ Face_recognition_ConvNets.py
@@ -121,3 +121,46 @@ def verify(image_path, identity, database, model):
 
     return dist, door_open
 
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the office by finding who is the person on the image_path image.
+
+    Arguments:
+        image_path -- path to an image
+        database -- database containing image encodings along with the name of the person on the image
+        model -- your Inception model instance in Keras
+
+    Returns:
+        min_dist -- the minimum distance between image_path encoding and the encodings from the database
+        identity -- string, the name prediction for the person on image_path
+    """
+
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above.
+    encoding = img_to_encoding(image_path, model)
+
+    ## Step 2: Find the closest encoding ##
+
+    # Initialize "min_dist" to a large value
+    min_dist = 100
+
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+
+        # Compute L2 distance between the target "encoding" and the current db_enc from the database.
+        dist = np.linalg.norm(encoding - db_enc)
+
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name.
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
+
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print("it's " + str(identity) + ", the distance is " + str(min_dist))
+
+    return min_dist, identity
+
+
+
